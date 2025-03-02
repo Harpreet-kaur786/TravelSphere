ac1116b06ac7b735a71f3e489b6dccd4c38e6e30
Harpreet Kaur <harpreet140507@gmail.com>
2025-03-02 16:48:31 -0500

    #10 Complete whole functionality of update name and update profile photo with styling
    Closes #10 #11

--- TravelSphere/components/HomeScreen/styles.js
+++ TravelSphere/components/HomeScreen/styles.js
@@ -1,27 +1,50 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    flexDirection: 'column',
   },
+  // Profile section (top left)
+profileContainer: {
+  flexDirection: 'row', 
+  alignItems: 'center',
+  marginBottom: 20,
+ 
+},
+
+profileImage: {
+  width: 70,
+  height: 70,
+  borderRadius: 50,
+  marginRight: 5, 
+},
+
+profileName: {
+  fontSize: 20,
+  fontWeight: 'bold',
+  marginLeft: 10,
+},
+editButton: {
+marginLeft: 10,
+},
   searchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#FFF',
-    borderRadius: 8,
+    borderRadius: 10,
     borderWidth: 1,
     borderColor: '#ddd',
-    paddingHorizontal: 10,
+    padding:5,
     marginBottom: 20,
   },
   input: {
     flex: 1,
     height: 40,
-    paddingLeft: 10,
+    paddingHorizontal: 10,
   },
   searchIcon: {
-    padding: 10,
+    marginLeft: 10,
   },
   filterContainer: {
     marginBottom: 20,
@@ -35,7 +58,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     color: '#333',
-    lineHeight: 22,
   },
   filterToggle: {
     padding: 5,
@@ -98,6 +120,58 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 14,
     color: '#555',
+  },
+  
+  // Modal styles
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  modalTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  uploadButton: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  uploadButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  saveButton: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    flex: 1,
+    marginRight: 10,
+    alignItems: 'center',
+  },
+  cancelButton: {
+    backgroundColor: '#f44336',
+    padding: 10,
+    borderRadius: 5,
+    flex: 1,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },  profileImagePreviewContainer: {
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
