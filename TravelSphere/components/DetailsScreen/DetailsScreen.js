b1431a3de2df928066dd19d4dc4d6ff00e79f658
m-bhalla236346 <monikabhalla1971508@gmail.com>
2025-04-13 14:56:10 -0400

    Update my screen
    I have update my code

--- TravelSphere/components/DetailsScreen/DetailsScreen.js
+++ TravelSphere/components/DetailsScreen/DetailsScreen.js
@@ -110,7 +110,7 @@ const DetailsScreen = ({ route, navigation }) => {
 
   // Map URL for static image
   const staticMapUrl = coordinates
-    ? `https://maps.gomaps.pro/maps/api/staticmap?center=${coordinates.latitude},${coordinates.longitude}&zoom=12&size=600x400&markers=color:red%7Clabel:A%7C${coordinates.latitude},${coordinates.longitude}&key=AlzaSyKCEnxPhd_T13y5e3WK0ouKaEeRoSGSG4R`
+    ? `https://maps.gomaps.pro/maps/api/staticmap?center=${coordinates.latitude},${coordinates.longitude}&zoom=12&size=600x400&markers=color:red%7Clabel:A%7C${coordinates.latitude},${coordinates.longitude}&key=AlzaSyb_J6PKounHNd1yQ2X78Petsn-LrtCwl5s`
     : null;
 
   // Function to mask the entire domain part of the email (including @ and after)
